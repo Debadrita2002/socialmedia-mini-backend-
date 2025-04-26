3a92bc29ce62a8d947645ce353a56cbf4bd2c1a0
Debadrita Dey <debadritadey08@gmail.com>
2025-04-26 18:56:39 +0530

    initial commit

--- app.js
+++ app.js
@@ -23,14 +23,48 @@ app.get("/login", (req,res) => {
     res.render('login');
 })
 app.get("/profile",isLoggedIn, async(req,res) => {
-    let user=await userModel.findOne({email: req.user.email})
-    console.log(user)
+    let user=await userModel.findOne({email: req.user.email}).populate("posts");
     res.render("profile",{user});
+})
+app.get("/edit", (req,res) => {
+    res.render('edit');
+})
+app.get("/like/:id",isLoggedIn, async(req,res) => {
+    let post=await postModel.findOne({_id: req.params.id}).populate("user")
+    if(post.likes.indexOf(req.user.userid)===-1){
+        post.likes.push(req.user.userid)
+    }
+    else{
+        post.likes.splice(post.likes.indexOf(req.user.userid),1)
+    }
+    await post.save()
+    res.redirect("/profile");
+})
+app.get("/edit/:id",isLoggedIn, async(req,res) => {
+    let post=await postModel.findOne({_id: req.params.id}).populate("user");
+    res.render("edit",{post})
+})
+app.post("/update/:id",isLoggedIn, async(req,res) => {
+    let post=await postModel.findOneAndUpdate({_id: req.params.id}, {content: req.body.content})
+    res.redirect("/profile")
 })
 
 app.get("/logout", (req,res) => {
     res.cookie("token","")
     res.redirect('/login');
+})
+
+app.post("/post",isLoggedIn, async(req,res) => {
+    let user=await userModel.findOne({email: req.user.email})
+    let {content} = req.body;
+    let post=await postModel.create({
+        user:user._id,
+        content
+    })
+    user.posts.push(post._id);
+    await user.save();
+    res.redirect("/profile")
+    
 })
 
 app.post("/register", async(req,res) => {
